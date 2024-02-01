f6eb1da907b50afd0ccd778976fd4c162a0de46e
ngarduque <157490533+ngarduque@users.noreply.github.com>
2024-02-01 15:32:10 +0800

    Update main.js

--- MyPortfolio_Nicole/main.js
+++ MyPortfolio_Nicole/main.js
@@ -25,16 +25,4 @@ function scrollHeader() {
 
 window.addEventListener('scroll', scrollHeader);
 
-const typed = document.querySelector('.typed');
-
-if(typed) {
-    let typed_strings = typed.getAttribute('data-typed-items');
-    typed_strings = typed_strings.split(',');
-    new typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000,
-    });
-}
+//scroll section
