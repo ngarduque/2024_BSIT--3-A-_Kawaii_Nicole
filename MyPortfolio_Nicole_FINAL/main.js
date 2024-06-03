470008c71f1467fe7341bc5c6c1c5b60ceb0782c
ngarduque <157490533+ngarduque@users.noreply.github.com>
2024-06-04 00:35:59 +0800

    Add files via upload
    final portfolio

---
+++ MyPortfolio_Nicole_FINAL/main.js
@@ -0,0 +1,30 @@
+const iconToggle = document.querySelector('.toggle_icon');
+const navbarMenu = document.querySelector('.nav_bar');
+const menuLinks = document.querySelector('.nav_bar_link');
+const iconClose = document.querySelector('.close_icon');
+
+iconToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+});
+
+iconClose.addEventListener('click', () => {
+    navbarMenu.classList.remove('active');
+});
+
+menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', () => {
+        navbarMenu.classList.remove('active');
+    });
+});
+
+//change bg header
+function scrollHeader() {
+    const header = document.getElementById('header');
+    this.scrollY >= 20 ? header.classList.add('active') : header.classList.remove('active');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
+
+
