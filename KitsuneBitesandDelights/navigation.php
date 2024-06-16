1eb98fb197953622cce345a04b5a123b2135ac92
ngarduque <157490533+ngarduque@users.noreply.github.com>
2024-06-16 19:54:07 +0800

    Delete KitsuneBitesandDelights directory

--- KitsuneBitesandDelights/navigation.php
+++
@@ -1,6 +0,0 @@
-<div class="btn-group ">
-    <a href="" class="btn btn-outline-light">Home</a>
-    <a href="" class="btn btn-outline-light">Shop</a>
-    <a href="" class="btn btn-outline-light">Contacts</a>
-    <a href="" class="btn btn-outline-light">About</a>
-</div>
